0952358873cd1dcfb6201b8f08460be18b445dc1
KedaarPawar <kedaarpawar2007@gmail.com>
2026-05-09 23:29:52 +0530

    Add date difference calculation script

---
+++ Practical 1/1.1.3.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+date1_str=input()
+date2_str=input()
+
+date1=datetime.strptime(date1_str, '%Y-%m-%d')
+date2=datetime.strptime(date2_str, '%Y-%m-%d')
+
+x=date2 - date1
+day_difference = x.days
+
+print(day_difference)
